98fdc6f5386217e8b5ffb2405981285cf78ab1b6
Mike Vercoelen <mike.vercoelen@gmail.com>
2019-11-26 21:59:22 +0100

    Basic setup for tracking online users

--- src/socketServer.js
+++ src/socketServer.js
@@ -1,15 +1,66 @@
 const socketIo = require('socket.io')
+const { getDecodedToken } = require('./utils/token')
+const User = require('./models/User')
+
+let onlineUsers = []
+
+const addOnlineUser = user => {
+  const hasUser = onlineUsers.some(u => u._id.toString() === user._id.toString())
+
+  if (hasUser) {
+    return onlineUsers
+  }
+
+  onlineUsers.push(user)
+}
+
+const removeOnlineUser = (user) => {
+  onlineUsers = onlineUsers.filter(onlineUser => user._id.toString() !== onlineUser._id.toString())
+  return onlineUsers
+}
 
 function initSocketServer (server) {
   const io = socketIo(server)
 
+  io.use(async (socket, next) => {
+    const token = socket.handshake.query.token
+
+    if (!token) {
+      return next(new Error('auth'))
+    }
+
+    const decodedToken = await getDecodedToken(token)
+
+    if (!decodedToken.userId) {
+      return next(new Error('auth'))
+    }
+
+    const { userId } = decodedToken
+
+    const user = await User
+      .findById(userId)
+      .lean()
+      .exec()
+
+    if (!user) {
+      return next(new Error('auth'))
+    }
+
+    delete user.password
+    delete user.__v
+
+    socket.user = user
+    next()
+  })
+
   io.on('connection', socket => {
-    const online = Object.keys(io.engine.clients)
-    io.emit('server message', JSON.stringify(online))
+    addOnlineUser(socket.user)
+
+    io.emit('server message', JSON.stringify(onlineUsers))
 
     socket.on('disconnect', () => {
-      const online = Object.keys(io.engine.clients)
-      io.emit('server message', JSON.stringify(online))
+      removeOnlineUser(socket.user)
+      io.emit('server message', JSON.stringify(onlineUsers))
     })
   })
 }
