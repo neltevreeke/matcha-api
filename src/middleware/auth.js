49b9779c38a88ea22841cdb2d3dde3bcfbfc0d9f
Mike Vercoelen <mike.vercoelen@gmail.com>
2020-01-24 16:14:21 +0100

    Fixed bug where expired JWT would crash the page

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,31 +10,35 @@ const authMiddleware = async (req, res, next) => {
     return next(error)
   }
 
-  const token = await getDecodedToken(tokenHeader)
+  try {
+    const token = await getDecodedToken(tokenHeader)
 
-  if (!token) {
-    const error = new Error('not-allowed')
-    error.statusCode = 401
-    return next(error)
+    if (!token) {
+      const error = new Error('not-allowed')
+      error.statusCode = 401
+      return next(error)
+    }
+
+    const user = await User
+      .findById(token.userId)
+      .lean()
+      .exec()
+
+    if (!user) {
+      const error = new Error('not-allowed')
+      error.statusCode = 401
+      return next(error)
+    }
+
+    delete user.password
+    delete user.__v
+
+    req.user = user
+
+    next()
+  } catch (error) {
+    next(error)
   }
-
-  const user = await User
-    .findById(token.userId)
-    .lean()
-    .exec()
-
-  if (!user) {
-    const error = new Error('not-allowed')
-    error.statusCode = 401
-    return next(error)
-  }
-
-  delete user.password
-  delete user.__v
-
-  req.user = user
-
-  next()
 }
 
 module.exports = authMiddleware
