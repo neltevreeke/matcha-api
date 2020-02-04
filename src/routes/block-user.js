3afea1b56b10932eee0befa72a1e6f0713e537bb
neltevreeke <nelte.p.vreeke@gmail.com>
2020-02-04 17:57:28 +0100

    updated block-user route with delete endpoint

--- src/routes/block-user.js
+++ src/routes/block-user.js
@@ -25,17 +25,46 @@ module.exports = app => {
     })
   })
 
-  app.post('/block-user', authMiddleware, async (req, res) => {
+  app.post('/block-user', authMiddleware, async (req, res, next) => {
     const userId = req.user._id.toString()
 
     const {
       blockedUserId
     } = req.body
 
-    Blocked.create({
-      userId,
-      blockedUserId
+    try {
+      await Blocked.create({
+        userId,
+        blockedUserId
+      })
+    } catch (e) {
+      const error = new Error('internal-server-error')
+      error.statusCode = 500
+      return next(error)
+    }
+
+    res.json({
+      blockedUsers: await getBlockedUsers(userId)
     })
+  })
+
+  app.delete('/block-user', authMiddleware, async (req, res, next) => {
+    const userId = req.user._id.toString()
+
+    const {
+      blockedUserId
+    } = req.body
+
+    try {
+      await Blocked.delete({
+        userId,
+        blockedUserId
+      })
+    } catch (e) {
+      const error = new Error('internal-server-error')
+      error.statusCode = 500
+      return next(error)
+    }
 
     res.json({
       blockedUsers: await getBlockedUsers(userId)
