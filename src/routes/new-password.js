032ca0045b5d06afd545c3b04a9853c729cf2d57
neltevreeke <nelte.p.vreeke@gmail.com>
2020-02-17 16:58:09 +0100

    we check the password reset token secret for extra security now too

--- src/routes/new-password.js
+++ src/routes/new-password.js
@@ -11,11 +11,15 @@ module.exports = app => {
     const salt = await bcrypt.genSalt(10)
     const hashPassword = await bcrypt.hash(password, salt)
 
-    await User.findOneAndUpdate({
-      passwordResetToken: token
-    }, {
-      password: hashPassword
-    })
+    const compareToken = await bcrypt.compare(process.env.PASSWORD_RESET_TOKEN_SECRET, token)
+
+    if (compareToken) {
+      await User.findOneAndUpdate({
+        passwordResetToken: token
+      }, {
+        password: hashPassword
+      })
+    }
 
     res.json({
       status: 200
